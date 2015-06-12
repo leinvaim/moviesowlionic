b3ed48a760c833e26176ae46ec148f8fb45c4a61
Craig McNamara <cmcnamara87@gmail.cok>
2015-06-12 19:01:51 +1000

    FUCK THIS THING!!!!!!!!! THIS HAS CAUSED SO MANY PROBLEMS!!!!

--- www/js/app.js
+++ www/js/app.js
@@ -42,9 +42,9 @@ app.config(function($stateProvider, $urlRouterProvider) {
             console.log('device is now ready!');
             // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
             // for form inputs)
-            if (window.cordova && window.cordova.plugins.Keyboard) {
-                cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-            }
+            // if (window.cordova && window.cordova.plugins.Keyboard) {
+            //     cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+            // }
             if (window.StatusBar) {
                 StatusBar.styleDefault();
             }
