fd57e2de539ba04d09c29d1aee1450a7ed7d1546
Craig McNamara <cmcnamara87@gmail.cok>
2015-06-19 22:43:09 +1000

    feat(ga): add google analytics to app.js

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,5 +1,19 @@
 'use strict';
 
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','http://www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-51312192-5', {
+    'storage' : 'none', // no cookies
+    'cookieDomain' :'none', // no domain
+    'clientId':'92bf24a5-20e5-4181-9778-2835f28c52d8'
+});
+ga('set', 'checkProtocolTask', null);
+ga('send', 'pageview', {'page': '/my/phone'});
+
+
 // Ionic Starter App
 
 // angular.module is a global place for creating, registering and retrieving Angular modules
@@ -18,6 +32,8 @@ angular.module('moviesowlApp', ['ionic'])
             StatusBar.styleDefault();
         }
     });
+
+
 })
 
 .config(function($stateProvider, $urlRouterProvider) {
