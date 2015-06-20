053006179f6c70e455766fbdf75245a4862db0e3
Kelvin Tamzil <kelvintamzil@yahoo.com>
2015-06-21 00:17:21 +1000

    add: angular moment

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -10,15 +10,16 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('moviesowlApp', ['ionic', 'angulartics', 'angulartics.google.analytics'])
+angular.module('moviesowlApp', ['ionic', 'angulartics', 'angulartics.google.analytics', 'angularMoment'])
 
-.run(function($ionicPlatform) {
+.run(function($ionicPlatform, amMoment) {
+    amMoment.changeLocale('en');
     $ionicPlatform.ready(function() {
 
         // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
         // for form inputs)
         if (window.cordova && window.cordova.plugins.Keyboard) {
-             cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
         }
         if (window.StatusBar) {
             StatusBar.styleDefault();
