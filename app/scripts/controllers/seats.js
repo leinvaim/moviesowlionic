36f849dc6eb803fc55389d909f7859eeb1352e4f
Kelvin Tamzil <kelvintamzil@yahoo.com>
2015-07-08 17:40:04 +1000

    add: show time to seats view

--- app/scripts/controllers/seats.js
+++ app/scripts/controllers/seats.js
@@ -8,17 +8,20 @@
  * Controller of the moviesowlApp
  */
 angular.module('moviesowlApp')
-    .controller('SeatsCtrl', function($scope, $http, $stateParams, showingsDataService) {
+    .controller('SeatsCtrl', function($scope, $http, $stateParams, showingsDataService, selectedMovieService) {
 
         //$http.get('http://api.moviesowl.com/v1/showings/' + $stateParams.showId).then(function(response) {
         //  $http.get('http://api.moviesowl.com/v1/showings/141061').then(function(response) {
 
         $scope.doRefresh = doRefresh;
+        $scope.movie = selectedMovieService.selectedMovie;
 
+        console.log(showingsDataService);
         var seatsData = _.find(showingsDataService.showingsData, function(showing) {
             return showing.id === parseInt($stateParams.showId);
         });
         console.log(seatsData);
+        $scope.session = seatsData;
         // var seatsData = response.data;
         $scope.seatingPlan = seatsData.seats;
 
