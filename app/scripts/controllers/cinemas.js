e3420d6730eb7dd48ce466ca078a708f98912a45
Craig McNamara <c.mcnamara@garvan.org.au>
2015-07-09 17:25:55 +1000

    refactor(build): remove all the basketjs stuff

--- app/scripts/controllers/cinemas.js
+++ app/scripts/controllers/cinemas.js
@@ -12,10 +12,6 @@ angular.module('moviesowlApp')
 
         console.log('In cinemas controller');
 
-        // $scope.doRefresh = doRefresh;
-        $scope.update = update;
-        $scope.reset = reset;
-
         // activate();
 
         // ////
@@ -28,78 +24,6 @@ angular.module('moviesowlApp')
         //     console.log('im here!');
         //     $scope.$broadcast('scroll.refreshComplete');
         // }
-
-       
-        function reload() {
-            console.log('Reloading in 2 seconds');
-            setTimeout(function() {
-                $ionicLoading.hide();
-                window.location.reload();
-            }, 1000);
-        }
-
-        function reset() {
-            console.log('Resetting');
-
-            $ionicLoading.show({
-                template: 'Loading...'
-            });
-
-            basket.clear();
-            reload();
-        }
-
-        function update() {
-            console.log('Reloading from Github');
-
-            $ionicLoading.show({
-                template: 'Loading...'
-            });
-
-
-            // use basket to reload from github
-            basket.clear();
-
-            var time = new Date().getTime();
-            var files = [{
-                url: 'http://leinvaim.github.io/moviesowlionic/scripts/vendor.js?time=' + time,
-                key: 'scripts/vendor.js',
-                execute: false
-            }, {
-                url: 'http://leinvaim.github.io/moviesowlionic/scripts/scripts.js?time=' + time,
-                key: 'scripts/scripts.js',
-                execute: false
-            }, {
-                url: 'http://leinvaim.github.io/moviesowlionic/scripts/templates.js?time=' + time,
-                key: 'scripts/templates.js',
-                execute: false
-            }, {
-                url: 'http://leinvaim.github.io/moviesowlionic/styles/vendor.css?time=' + time,
-                key: 'styles/vendor.css',
-                execute: false
-            }, {
-                url: 'http://leinvaim.github.io/moviesowlionic/styles/style.css?time=' + time,
-                key: 'styles/style.css',
-                execute: false
-            }];
-
-            console.log('Files to load');
-            console.log(files);
-
-            basket.require.apply(null, files).then(function(stuff) {
-                console.log('New files cached, about to reload');
-                console.log('Templates:');
-                console.log(basket.get('scripts/templates.js'));
-                console.log('RELOAD FILES:');
-                console.log(stuff);
-
-                $scope.$broadcast('scroll.refreshComplete');
-                console.log('FUUUCCCCCKKKK');
-                reload();
-            }, function() {
-                console.log('Failed to get from Github!');
-            });
-        }
 
         // function doRefresh() {
         //     activate();
