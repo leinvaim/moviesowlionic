9a67997d8df3d478a6b1ab318f075759327f1274
Craig McNamara <cmcnamara87@gmail.com>
2015-08-22 14:18:48 +1000

    Updates

---
+++ scripts/app.js
@@ -0,0 +1,72 @@
+'use strict';
+
+
+
+//ga('send', 'pageview', {'page': '/my/phone'});
+
+
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+angular.module('moviesowlApp', ['ionic', 'angulartics', 'angulartics.google.analytics', 'angularMoment', 'ion-affix'])
+
+.run(function($ionicPlatform, amMoment, $rootScope) {
+    amMoment.changeLocale('en');
+    $ionicPlatform.ready(function() {
+
+        $ionicPlatform.ready(function() {
+            if(navigator.splashscreen){
+                setTimeout(function() {
+                    navigator.splashscreen.hide();
+                }, 100);
+            }
+        });
+
+        // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+        // for form inputs)
+        if (window.cordova && window.cordova.plugins.Keyboard) {
+            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+        }
+        if (window.StatusBar) {
+            StatusBar.styleDefault();
+            //console.log('statusbar', StatusBar);
+            //StatusBar.overlaysWebView(true);
+            //StatusBar.style(1); //Light
+        }
+    });
+
+
+})
+
+.config(function($stateProvider, $urlRouterProvider) {
+    console.log('Config running, adding routes!');
+    $urlRouterProvider.otherwise('/movies');
+    $stateProvider
+        // .state('cinemas', {
+        //     url: '/cinemas',
+        //     templateUrl: 'templates/cinemas.html',
+        //     controller: 'CinemasCtrl'
+        // })
+        .state('movies', {
+            url: '/movies',
+            templateUrl: 'templates/movies.html',
+            controller: 'MoviesCtrl'
+        })
+        .state('movie', {
+            url: '/movie/:movieId',
+            templateUrl: 'templates/movieDetails.html',
+            controller: 'MovieDetailsCtrl'
+        })
+        .state('showings', {
+            url: '/showings/:movieId',
+            templateUrl: 'templates/showings.html',
+            controller: 'ShowingsCtrl'
+        })
+        .state('seats', {
+            url: '/seats/:showId',
+            templateUrl: 'templates/seats.html',
+            controller: 'SeatsCtrl'
+        });
+});
