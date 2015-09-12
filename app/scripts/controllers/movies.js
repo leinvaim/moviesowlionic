92e3d51cd97e58a4e02749e561e4ecf80e9e00d6
Kelvin Tamzil <kelvintamzil@yahoo.com>
2015-09-12 15:20:50 +1000

    add: update progress bar

--- app/scripts/controllers/movies.js
+++ app/scripts/controllers/movies.js
@@ -216,4 +216,59 @@ angular.module('moviesowlApp')
             $scope.mode = mode;
             localStorage.viewMode = mode;
         }
+
+
+
+        var appCache = window.applicationCache;
+
+        function handleCacheEvent(e) {
+            console.log('haha');
+            console.log(e);
+        }
+
+        function handleCacheError(e) {
+            alert('Error: Cache failed to update!');
+        };
+
+        function updateFound(e) {
+            console.log(e);
+            console.log('update found');
+            $scope.$apply(function(){
+                $scope.hasUpdate = true;
+            });
+        }
+        
+        function getProgress(e) {
+            console.log(e);
+            console.log('progress');
+            $scope.$apply(function(){
+                $scope.progressWidth = e.load / e.total * 100;
+            });
+        }
+
+// Fired after the first cache of the manifest.
+        appCache.addEventListener('cached', handleCacheEvent, false);
+
+// Checking for an update. Always the first event fired in the sequence.
+        appCache.addEventListener('checking', handleCacheEvent, false);
+
+// An update was found. The browser is fetching resources.
+        appCache.addEventListener('downloading', updateFound, false);
+
+// The manifest returns 404 or 410, the download failed,
+// or the manifest changed while the download was in progress.
+        appCache.addEventListener('error', handleCacheError, false);
+
+// Fired after the first download of the manifest.
+        appCache.addEventListener('noupdate', handleCacheEvent, false);
+
+// Fired if the manifest file returns a 404 or 410.
+// This results in the application cache being deleted.
+        appCache.addEventListener('obsolete', handleCacheEvent, false);
+
+// Fired for each resource listed in the manifest as it is being fetched.
+        appCache.addEventListener('progress', getProgress, false);
+
+// Fired when the manifest resources have been newly redownloaded.
+        appCache.addEventListener('updateready', handleCacheEvent, false);
     });
