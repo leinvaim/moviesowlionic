2f98e054b29336f6d25fd77443e867c5adb28cc3
Kelvin Tamzil <kelvintamzil@yahoo.com>
2015-09-17 10:40:11 +1000

    Updates

---
+++ scripts/configuration.js
@@ -0,0 +1,7 @@
+"use strict";
+
+ angular.module('config', [])
+
+.constant('ENV', {name:'production',apiEndpoint:'http://128.199.104.251/moviesowl.com/current/public/api/v1/',posterEndpoint:'http://128.199.104.251/moviesowl.com/current/public/'})
+
+;
