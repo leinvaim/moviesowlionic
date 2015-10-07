e870ba2370fc23a451a70ec76b48fa90bb52239c
Craig McNamara <cmcnamara87@gmail.com>
2015-10-07 16:44:22 +1000

    Updates

---
+++ scripts/controllers/seats.js
@@ -0,0 +1,64 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name moviesowlApp.controller:SeatsCtrl
+ * @description
+ * # SeatsCtrl
+ * Controller of the moviesowlApp
+ */
+angular.module('moviesowlApp')
+    .controller('SeatsCtrl', function(ENV, $scope, $http, $stateParams, showingsDataService, selectedMovieService,
+                                      $timeout) {
+
+        //$http.get('http://api.moviesowl.com/v1/showings/' + $stateParams.showId).then(function(response) {
+        //  $http.get('http://api.moviesowl.com/v1/showings/141061').then(function(response) {
+
+        $scope.doRefresh = doRefresh;
+        $scope.movie = selectedMovieService.selectedMovie;
+        $scope.buyTickets = buyTickets;
+        $scope.buyTelstraTickets = buyTelstraTickets;
+
+        $scope.$on('$ionicView.afterEnter', function(){
+            activate();
+        });
+
+        function activate() {
+            var seatsData = _.find(showingsDataService.showingsData, function(showing) {
+                return showing.id === parseInt($stateParams.showId);
+            });
+            $scope.session = seatsData;
+            $timeout(function() {
+                getSeatingPlan(seatsData);
+            }, 1000);
+        }
+
+        function getSeatingPlan(seatsData) {
+            $scope.seatingPlan = seatsData.seats;
+            var numOfSeatInRow = $scope.seatingPlan[0].length;
+            $scope.seatWidth = 100 / numOfSeatInRow;
+        }
+
+
+
+        // console.log(seatsData);
+        function doRefresh() {
+            console.log('Reloading Seats');
+            $http.get(ENV.apiEndpoint + 'showings/' + $stateParams.showId).then(function(response) {
+                $scope.seatingPlan = response.data.seats;
+                $scope.$broadcast('scroll.refreshComplete');
+            }, function() {
+                $scope.$broadcast('scroll.refreshComplete');
+            });
+        }
+
+        function buyTickets() {
+            var url = 'https://www.eventcinemas.com.au';
+            window.open(url, '_system');
+        }
+
+        function buyTelstraTickets() {
+            var url = 'https://www.my.telstra.com.au/myaccount/home?goto=https%3A%2F%2Fwww.my.telstra.com.au%2Fmyaccount%2Floyalty-offers-consumer';
+            window.open(url, '_system');
+        }
+    });
---
+++ scripts/tracking.js
@@ -0,0 +1,28 @@
+// jshint ignore: start
+(function() {
+
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','http://www.google-analytics.com/analytics.js','ga');
+
+    localStorage.gaClientId = localStorage.gaClientId || guid();
+
+    ga('create', 'UA-51312192-5', {
+        'storage' : 'none', // no cookies
+        'cookieDomain' :'none', // no domain
+        'clientId': localStorage.gaClientId
+    });
+    ga('set', 'checkProtocolTask', null);
+
+    function guid() {
+        function s4() {
+            return Math.floor((1 + Math.random()) * 0x10000)
+                .toString(16)
+                .substring(1);
+        }
+        return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+            s4() + '-' + s4() + s4() + s4();
+    }
+
+})();
