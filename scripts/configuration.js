c21bbd4a8c6e114307f78f478506d1aaaeeda7d3
Craig McNamara <cmcnamara87@gmail.com>
2015-10-11 09:52:50 +1000

    Updates

--- scripts/configuration.js
+++ scripts/configuration.js
@@ -2,6 +2,6 @@
 
  angular.module('config', [])
 
-.constant('ENV', {name:'production',apiEndpoint:'http://128.199.104.251/moviesowl.com/current/public/api/v1/',posterEndpoint:'http://128.199.104.251/moviesowl.com/current/public/'})
+.constant('ENV', {name:'production',apiEndpoint:'http://moviesowl.com/api/v1/',posterEndpoint:'http://moviesowl.com/'})
 
 ;
