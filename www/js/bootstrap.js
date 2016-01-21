2fd2452e758eb2d694f4490bfcacd5045929f89c
Craig McNamara <cmcnamara87@gmail.com>
2016-01-21 10:30:56 +1000

    fix bootstrap only runs on localhost:8100

--- www/js/bootstrap.js
+++ www/js/bootstrap.js
@@ -1,4 +1,4 @@
-(function(){
+(function () {
 // Retrieved and slightly modified from: https://github.com/typicode/pegasus
 // --------------------------------------------------------------------------
 //
@@ -16,7 +16,7 @@
         // onSuccess handler
         // onError   handler
         // cb        placeholder to avoid using var, should not be used
-        xhr.onreadystatechange = xhr.then = function(onSuccess, onError, cb) {
+        xhr.onreadystatechange = xhr.then = function (onSuccess, onError, cb) {
 
             // Test if onSuccess is a function or a load event
             if (onSuccess.call) a = [onSuccess, onError];
@@ -27,11 +27,11 @@
                 // index will be:
                 // 0 if status is between 0 and 399
                 // 1 if status is over
-                cb = a[0|xhr.status / 400];
+                cb = a[0 | xhr.status / 400];
 
                 // Safari doesn't support xhr.responseType = 'json'
                 // so the response is parsed
-                if (cb) cb(xhr.status === 200 || xhr.status === 0?JSON.parse(xhr.responseText):xhr);
+                if (cb) cb(xhr.status === 200 || xhr.status === 0 ? JSON.parse(xhr.responseText) : xhr);
             }
         };
 
@@ -43,21 +43,21 @@
     };
 //------------------------------------------------------------------
 // Step 2: After fetching manifest (localStorage or XHR), load it
-    function loadManifest(manifest,fromLocalStorage,timeout){
+    function loadManifest(manifest, fromLocalStorage, timeout) {
         // Safety timeout. If BOOTSTRAP_OK is not defined,
         // it will delete the 'localStorage' version and revert to factory settings.
-        if(fromLocalStorage){
-            setTimeout(function(){
-                if(!window.BOOTSTRAP_OK){
+        if (fromLocalStorage) {
+            setTimeout(function () {
+                if (!window.BOOTSTRAP_OK) {
                     console.warn('BOOTSTRAP_OK !== true; Resetting to original manifest.json...');
                     localStorage.removeItem('manifest');
                     location.reload();
                 }
-            },timeout);
+            }, timeout);
         }
 
-        if(!manifest.load) {
-            console.error('Manifest has nothing to load (manifest.load is empty).',manifest);
+        if (!manifest.load) {
+            console.error('Manifest has nothing to load (manifest.load is empty).', manifest);
             return;
         }
 
@@ -67,21 +67,21 @@
             now = Date.now();
 
         // Load Scripts
-        function loadScripts(){
-            scripts.forEach(function(src) {
-                if(!src) return;
+        function loadScripts() {
+            scripts.forEach(function (src) {
+                if (!src) return;
                 // Ensure the 'src' has no '/' (it's in the root already)
-                if(src[0] === '/') src = src.substr(1);
-                src = manifest.root + src ;
+                if (src[0] === '/') src = src.substr(1);
+                src = manifest.root + src;
                 // Load javascript
-                if(src.substr(-3) === ".js"){
-                    el= document.createElement('script');
-                    el.type= 'text/javascript';
-                    el.src= src + '?' + now;
+                if (src.substr(-3) === ".js") {
+                    el = document.createElement('script');
+                    el.type = 'text/javascript';
+                    el.src = src + '?' + now;
                     el.async = false;
                     // Load CSS
                 } else {
-                    el= document.createElement('link');
+                    el = document.createElement('link');
                     el.rel = "stylesheet";
                     el.href = src + '?' + now;
                     el.type = "text/css";
@@ -93,18 +93,18 @@
         //---------------------------------------------------
         // Step 3: Ensure the 'root' end with a '/'
         manifest.root = manifest.root || './';
-        if(manifest.root.length > 0 && manifest.root[manifest.root.length-1] !== '/')
+        if (manifest.root.length > 0 && manifest.root[manifest.root.length - 1] !== '/')
             manifest.root += '/';
 
         // Step 4: Save manifest for next time
-        if(!fromLocalStorage) {
+        if (!fromLocalStorage) {
             console.log('Bootstrap: New manfiest', manifest);
             localStorage.setItem('manifest', JSON.stringify(manifest));
         }
 
         // Step 5: Load Scripts
         // If we're loading Cordova files, make sure Cordova is ready first!
-        if(typeof window.cordova !== 'undefined'){
+        if (typeof window.cordova !== 'undefined') {
             document.addEventListener("deviceready", loadScripts, false);
         } else {
             loadScripts();
@@ -112,23 +112,28 @@
         // Save to global scope
         window.Manifest = manifest;
     }
+
 //---------------------------------------------------------------------
     window.Manifest = {};
-    //localStorage.removeItem('manifest');
-// Step 1: Load manifest from localStorage
+
+    // Clear if on my local dev machine
+    if (location.origin === 'http://localhost:8100') {
+        localStorage.removeItem('manifest');
+    }
+    // Step 1: Load manifest from localStorage
     var manifest = JSON.parse(localStorage.getItem('manifest'));
     console.log('stored manfiest', manifest);
-// grab manifest.json location from <script manifest="..."></script>
+    // grab manifest.json location from <script manifest="..."></script>
     var s = document.querySelector('script[manifest]');
-// Not in localStorage? Fetch it!
-    if(!manifest){
-        var url = (s? s.getAttribute('manifest'): null) || 'manifest.json';
+    // Not in localStorage? Fetch it!
+    if (!manifest) {
+        var url = (s ? s.getAttribute('manifest') : null) || 'manifest.json';
         // get manifest.json, then loadManifest.
-        pegasus(url).then(loadManifest,function(xhr){
-            console.error('Could not download '+url+': '+xhr.status);
+        pegasus(url).then(loadManifest, function (xhr) {
+            console.error('Could not download ' + url + ': ' + xhr.status);
         });
-// Manifest was in localStorage. Load it immediatly.
+    // Manifest was in localStorage. Load it immediatly.
     } else {
-        loadManifest(manifest,true,s.getAttribute('timeout') || 10000);
+        loadManifest(manifest, true, s.getAttribute('timeout') || 10000);
     }
 })();
